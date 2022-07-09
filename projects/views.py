4f02b39bea26f86afce89fdf99f0bcac3f5e7ebb
Kateryna <kateryna.kvasniak@gmail.com>
2022-07-09 16:49:30 +0200

    Render data to a single-project template

--- projects/views.py
+++ projects/views.py
@@ -1,8 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+projectsList = [
+    {
+        'id': '1',
+        'title': 'Ecommerce Website',
+        'description': 'Fully functional ecommerce website'
+    },
+    {
+        'id': '2',
+        'title': 'Portfolio Website',
+        'description': 'A personal website to write articles and display work'
+    },
+    {
+        'id': '3',
+        'title': 'Social Network',
+        'description': 'An open source project built by the community'
+    }
+]
+
 def projects(request):
-    return render(request, 'projects/projects.html')
+    context = {'projectsList': projectsList}
+    return render(request, 'projects/projects.html', context)
+    
 
 def project(request, pk):
-    return render(request, 'projects/single-project.html')
+    project = None
+    for element in projectsList:
+        if element['id'] == pk:
+            project = element
+    return render(request, 'projects/single-project.html', {'project': project})
