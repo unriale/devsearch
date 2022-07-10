76e6219a3433ecdb2d542cd2794b0fe837b200da
Kateryna <kateryna.kvasniak@gmail.com>
2022-07-10 07:53:08 +0200

    Added Review and Tag models

--- projects/models.py
+++ projects/models.py
@@ -8,6 +8,36 @@ class Project(models.Model):
     source_link = models.CharField(max_length=2000, null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(unique=True, primary_key=True, default=uuid.uuid4, editable=False)
+    tags = models.ManyToManyField('Tag', blank=True)
+    vote_total = models.IntegerField(default=0, null=True, blank=True)
+    vote_ratio = models.IntegerField(default=0, null=True, blank=True)
 
     def __str__(self):
         return self.title
+
+
+class Review(models.Model):
+    VOTE_TYPE = ( 
+        ('up', 'Up Vote'),
+        ('down', 'Down Vote'),
+    )
+    id = models.UUIDField(unique=True, primary_key=True, default=uuid.uuid4, editable=False)
+    # owner
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    body = models.TextField(null=True, blank=True)
+    value = models.CharField(choices=VOTE_TYPE, max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.value
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(unique=True, primary_key=True, editable=False, default=uuid.uuid4)
+
+    def __str__(self):
+        return self.name
+
+
--- projects/views.py
+++ projects/views.py
@@ -1,32 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-projectsList = [
-    {
-        'id': '1',
-        'title': 'Ecommerce Website',
-        'description': 'Fully functional ecommerce website'
-    },
-    {
-        'id': '2',
-        'title': 'Portfolio Website',
-        'description': 'A personal website to write articles and display work'
-    },
-    {
-        'id': '3',
-        'title': 'Social Network',
-        'description': 'An open source project built by the community'
-    }
-]
+from .models import Project
+
 
 def projects(request):
-    context = {'projectsList': projectsList}
+    projects = Project.objects.all()
+    context = {'projectsList': projects}
     return render(request, 'projects/projects.html', context)
     
 
 def project(request, pk):
-    project = None
-    for element in projectsList:
-        if element['id'] == pk:
-            project = element
-    return render(request, 'projects/single-project.html', {'project': project})
+    project = Project.objects.get(pk=pk)
+    tags = project.tags.all()
+    return render(request, 'projects/single-project.html', { 'project': project, 'tags': tags })
