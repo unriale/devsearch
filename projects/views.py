d097f39cbdafb625e01b37389bec4380236ca0a5
Kateryna <kateryna.kvasniak@gmail.com>
2022-07-10 09:16:35 +0200

    Added CRUD for Projects

--- projects/views.py
+++ projects/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
 
 from .models import Project
+from .forms import ProjectForm
 
 
 def projects(request):
@@ -14,3 +14,38 @@ def project(request, pk):
     project = Project.objects.get(pk=pk)
     tags = project.tags.all()
     return render(request, 'projects/single-project.html', { 'project': project, 'tags': tags })
+
+
+def createProject(request):
+    form = ProjectForm()
+    if request.method == 'POST':
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+
+    context = {'form': form}
+    return render(request, 'projects/project-form.html', context)
+
+
+def updateProject(request, pk):
+    project = Project.objects.get(id=pk)
+    form = ProjectForm(instance=project)
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, instance=project)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+    
+    context = {'form': form}
+    return render(request, 'projects/project-form.html', context)
+
+
+def deleteProject(request, pk):
+    project = Project.objects.get(id=pk)
+    if request.method == 'POST':
+        project.delete()
+        return redirect('projects')
+    context = {'object': project }
+    return render(request, 'projects/delete-template.html', context)
+    
