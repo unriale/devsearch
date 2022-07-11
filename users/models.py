48c344fc36e87ee78cb2bf842cc5eed1222f2e0f
Kateryna <kateryna.kvasniak@gmail.com>
2022-07-11 20:05:44 +0200

    Add & Render profiles

--- users/models.py
+++ users/models.py
@@ -8,6 +8,7 @@ class Profile(models.Model):
     email = models.EmailField(max_length=500, blank=True, null=True)
     username = models.CharField(max_length=200, blank=True, null=True)
     short_intro = models.CharField(max_length=200, blank=True, null=True)
+    location = models.CharField(max_length=200, blank=True, null=True)
     bio = models.TextField(blank=True, null=True)
     profile_image = models.ImageField(null=True, blank=True, upload_to='profiles/', default='profiles/user-default.png')
     social_github = models.CharField(max_length=200, blank=True, null=True)
@@ -22,3 +23,12 @@ class Profile(models.Model):
         return str(self.user.username)
 
 
+class Skill(models.Model):
+    owner = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=200, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    id = models.UUIDField(unique=True, primary_key=True, default=uuid.uuid4, editable=False)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.name)
